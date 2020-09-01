2eda1f69804d31c4264ebe9c4ac6ac882abb4d57
john <johnmelton97@gmail.com>
2020-09-01 12:19:27 -0400

    Refined Code

--- blocky.py
+++ blocky.py
@@ -25,147 +25,145 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 CUBE_COUNT = 85
-player_size = 20 
+player_size = 20
 
-#Game state
+# Game state
 state = 0
 score = 0
 
-player_pos = [WIDTH/2, HEIGHT - 4 * player_size]
+player_pos = [WIDTH / 2, HEIGHT - 4 * player_size]
 
-#Create game screen
+# Create game screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-#Create score text
+# Create score text
 font = pygame.font.Font('freesansbold.ttf', 32)
 textX = 10
 textY = 10
 
 enemy_list = []
 
+
 def show_score(x, y):
     score_text = font.render("Score: " + str(score), True, WHITE)
     screen.blit(score_text, (x, y))
 
 
-def make_enemy(cube_list, WIDTH):
+def make_enemy(cube_list, width):
+    if len(cube_list) == 0:
+        for x in range(CUBE_COUNT):
+            cube = Obstacle(width)
+            cube_list.append(cube)
 
-        if len(cube_list) == 0:
-            for x in range(CUBE_COUNT):
-                cube = Obstacle(WIDTH)
-                cube_list.append(cube)
-            
 
 def display_death_screen():
     screen.fill(BLACK)
-    show_score(WIDTH/2.37, HEIGHT/2)
+    show_score(WIDTH / 2.37, HEIGHT / 2)
     death_text = font.render("YOU DIED", True, WHITE)
-    screen.blit(death_text, (WIDTH/2.35, HEIGHT/2 - 50))
+    screen.blit(death_text, (WIDTH / 2.35, HEIGHT / 2 - 50))
 
 
 def check_collision(player_left_side, player_top_side, block_left_side, block_top_side, p_size, c_size):
+    player_right_side = player_left_side + p_size
+    player_bottom_side = player_top_side + p_size
 
-        player_right_side = player_left_side + p_size
-        player_bottom_side = player_top_side + p_size
+    block_right_side = block_left_side + c_size
+    block_bottom_side = block_top_side + c_size
 
-        block_right_side = block_left_side + c_size
-        block_bottom_side = block_top_side + c_size
+    # Block is to the left of the player and top of block is higher than player's top
+    if ((block_left_side <= player_left_side <= block_right_side
+         and block_top_side <= player_top_side <= block_bottom_side and block_top_side <= player_bottom_side)
 
-        #Block is to the left of the player and top of block is higher than player's top
-        if ((block_left_side <= player_left_side and block_right_side >= player_left_side
-        and block_top_side <= player_top_side and block_bottom_side >= player_top_side and block_top_side <= player_bottom_side)
+            # Block is to the right of the player and top of block is higher than player's top
+            or (block_left_side <= player_right_side <= block_right_side
+                and block_top_side >= player_top_side and block_bottom_side >= player_top_side and block_top_side <= player_bottom_side)
 
-        #Block is to the right of the player and top of block is higher than player's top
-        or (block_left_side <= player_right_side and block_right_side >= player_right_side
-        and block_top_side >= player_top_side and block_bottom_side >= player_top_side and block_top_side <= player_bottom_side)
+            # Block is to the left of the player and top of block is lower than player's top
+            or (block_left_side <= player_left_side <= block_right_side
+                and block_top_side >= player_top_side and block_bottom_side >= player_top_side and block_top_side <= player_bottom_side)
 
-        #Block is to the left of the player and top of block is lower than player's top
-        or (block_left_side <= player_left_side and block_right_side >= player_left_side
-        and block_top_side >= player_top_side and block_bottom_side >= player_top_side and block_top_side <= player_bottom_side)
+            # Block is to the right of the player and top of block is lower than player's top
+            or (block_left_side <= player_right_side <= block_right_side
+                and block_top_side <= player_top_side <= block_bottom_side and block_top_side <= player_bottom_side)
 
-        #Block is to the right of the player and top of block is lower than player's top
-        or (block_left_side <= player_right_side and block_right_side >= player_right_side
-        and block_top_side <= player_top_side and block_bottom_side >= player_top_side and block_top_side <= player_bottom_side)
+            # BLOCK IN PLAYER
+            or (block_left_side >= player_left_side and block_right_side <= player_right_side
+                and block_top_side >= player_top_side and block_bottom_side >= player_top_side and block_top_side <= player_bottom_side)
 
-        #BLOCK IN PLAYER
-        or (block_left_side >= player_left_side and block_right_side <= player_right_side
-        and block_top_side >= player_top_side and block_bottom_side >= player_top_side and block_top_side <= player_bottom_side)
+            # BLOCK IN PLAYER
+            or (block_left_side >= player_left_side and block_right_side <= player_right_side
+                and block_top_side >= player_top_side and block_bottom_side >= player_top_side and block_top_side <= player_bottom_side)):
 
-        #BLOCK IN PLAYER
-        or (block_left_side >= player_left_side and block_right_side <= player_right_side
-        and block_top_side >= player_top_side and block_bottom_side >= player_top_side and block_top_side <= player_bottom_side)):
+        return 2
 
-            return 2
-
-        else:
-            return 1      
+    else:
+        return 1
 
 
-def update_list(cube_list, WIDTH):
-    #index = length of list
-    if len(cube_list) < CUBE_COUNT:    
-        cube = Obstacle(WIDTH)
+def update_list(cube_list, width):
+    # index = length of list
+    if len(cube_list) < CUBE_COUNT:
+        cube = Obstacle(width)
         cube_list.append(cube)
 
 
 def draw_enemies(cube_list):
-
     for x in range(len(cube_list)):
-        pygame.draw.rect(screen, cube_list[x].color, (cube_list[x].x_pos, cube_list[x].y_pos, cube_list[x].size, cube_list[x].size), 0)
+        pygame.draw.rect(screen, cube_list[x].color,
+                         (cube_list[x].x_pos, cube_list[x].y_pos, cube_list[x].size, cube_list[x].size), 0)
 
 
-def move_enemy(cube_list, score):
-
+def move_enemy(cube_list, scr):
     for x in range(len(cube_list)):
 
-        if(cube_list[x].y_pos > player_pos[1]):
-            score += 1
+        if cube_list[x].y_pos > player_pos[1]:
+            scr += 1
 
         if cube_list[x].y_pos < HEIGHT:
 
-            if(cube_list[x].y_pos < HEIGHT / 2):
+            if cube_list[x].y_pos < HEIGHT / 2:
                 cube_list[x].size = 1
-                cube_list[x].y_pos += 5 
-            
+                cube_list[x].y_pos += 5
+
             else:
-                cube_list[x].y_pos += 5 * (cube_list[x].size * .1) # acceleration of cube
-                cube_list[x].size += (cube_list[x].size) * .03 # = scaling cubes
+                cube_list[x].y_pos += 5 * (cube_list[x].size * .1)  # acceleration of cube
+                cube_list[x].size += cube_list[x].size * .03  # = scaling cubes
 
         else:
             cube_list.pop(x)
             update_list(cube_list, WIDTH)
-        
-    return score
+
+    return scr
 
 
-def start_screen(state, clicked, enter):
+def start_screen(clicked, enter):
     mouse = pygame.mouse.get_pos()
-
+    sta = 0
     screen.fill(BLACK)
-    font = pygame.font.Font('freesansbold.ttf', 64)
-    death_text = font.render("Cubefield 2D", True, WHITE)
-    screen.blit(death_text, (WIDTH/3.3, HEIGHT/2 - 100))
+    fnt = pygame.font.Font('freesansbold.ttf', 64)
+    death_text = fnt.render("Cubefield 2D", True, WHITE)
+    screen.blit(death_text, (WIDTH / 3.3, HEIGHT / 2 - 100))
 
-    font = pygame.font.Font('freesansbold.ttf', 32)
-    death_text = font.render("By John Melton", True, WHITE)
-    screen.blit(death_text, (WIDTH/2.7, HEIGHT/2))
+    fnt = pygame.font.Font('freesansbold.ttf', 32)
+    death_text = fnt.render("By John Melton", True, WHITE)
+    screen.blit(death_text, (WIDTH / 2.7, HEIGHT / 2))
 
-    #GREEN BUTTON
-    restart_text = font.render("Start", True, WHITE)
-    textRect = (250 + 12, (HEIGHT - 250)+(50/4))
-    
-    if (250+100) > mouse[0] > 250 and (HEIGHT - 250 + 50) > mouse[1] > (HEIGHT - 250):
+    # GREEN BUTTON
+    restart_text = fnt.render("Start", True, WHITE)
+    text_rect = (250 + 12, (HEIGHT - 250) + (50 / 4))
+
+    if (250 + 100) > mouse[0] > 250 and (HEIGHT - 250 + 50) > mouse[1] > (HEIGHT - 250):
         rect_start = pygame.draw.rect(screen, LIGHT_GREEN, (250, HEIGHT - 250, 100, 50), 0)
         if clicked:
-            state = 1
+            sta = 1
     else:
         rect_start = pygame.draw.rect(screen, GREEN, (250, HEIGHT - 250, 100, 50), 0)
-    screen.blit(restart_text, textRect)
+    screen.blit(restart_text, text_rect)
 
-    #RED BUTTON
-    restart_text = font.render("Quit", True, WHITE)
-    textRect = (WIDTH - 350 + 15 , (HEIGHT - 250)+(50/4))
-    
+    # RED BUTTON
+    restart_text = fnt.render("Quit", True, WHITE)
+    text_rect = (WIDTH - 350 + 15, (HEIGHT - 250) + (50 / 4))
+
     if (WIDTH - 350 + 100) > mouse[0] > (WIDTH - 350) and (HEIGHT - 250 + 50) > mouse[1] > (HEIGHT - 250):
         rect_stop = pygame.draw.rect(screen, LIGHT_RED, (WIDTH - 350, HEIGHT - 250, 100, 50), 0)
         if clicked:
@@ -173,17 +171,16 @@ def start_screen(state, clicked, enter):
             exit()
     else:
         rect_stop = pygame.draw.rect(screen, RED, (WIDTH - 350, HEIGHT - 250, 100, 50), 0)
-    screen.blit(restart_text, textRect)
+    screen.blit(restart_text, text_rect)
 
     pygame.display.update()
     if enter:
-        state = 1
-        score = 0
+        sta = 1
         print("ENTER")
-    return state
+    return sta
 
 
-def run_game(cube_list, score):
+def run_game(cube_list, scr):
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_LEFT] and (player_pos[0] > 0):
@@ -192,15 +189,16 @@ def run_game(cube_list, score):
         player_pos[0] += 6
 
     screen.fill(BLACK)
-    
-    #Move the enemies
+
+    # Move the enemies
     make_enemy(enemy_list, WIDTH)
     draw_enemies(enemy_list)
-    score = move_enemy(enemy_list, score)
+    scr = move_enemy(enemy_list, scr)
 
     for x in range(len(cube_list)):
-        state = check_collision(player_pos[0], player_pos[1], cube_list[x].x_pos, cube_list[x].y_pos, player_size, cube_list[x].size)
-        if state == 2:
+        sta = check_collision(player_pos[0], player_pos[1], cube_list[x].x_pos, cube_list[x].y_pos, player_size,
+                              cube_list[x].size)
+        if sta == 2:
             enemy_list.clear()
             break
         else:
@@ -209,51 +207,52 @@ def run_game(cube_list, score):
             show_score(textX, textY)
 
     pygame.display.update()
-        
-    return state, score
+
+    return sta, scr
 
 
-def dead_player(score, clicked, enter):
+def dead_player(scr, clicked, enter):
     mouse = pygame.mouse.get_pos()
-    state = 2
+    sta = 2
     display_death_screen()
 
-    #GREEN BUTTON
+    # GREEN BUTTON
     restart_text = font.render("Start", True, WHITE)
-    textRect = (250 + 12, (HEIGHT - 250)+(50/4))
-    
-    if (250+100) > mouse[0] > 250 and (HEIGHT - 250 + 50) > mouse[1] > (HEIGHT - 250):
+    text_rect = (250 + 12, (HEIGHT - 250) + (50 / 4))
+
+    if (250 + 100) > mouse[0] > 250 and (HEIGHT - 250 + 50) > mouse[1] > (HEIGHT - 250):
         rect_start = pygame.draw.rect(screen, LIGHT_GREEN, (250, HEIGHT - 250, 100, 50), 0)
         if clicked:
-            state = 1
-            score = 0
+            sta = 1
+            scr = 0
     else:
         rect_start = pygame.draw.rect(screen, GREEN, (250, HEIGHT - 250, 100, 50), 0)
-    screen.blit(restart_text, textRect)
+    screen.blit(restart_text, text_rect)
 
-    #RED BUTTON
+    # RED BUTTON
     restart_text = font.render("Quit", True, WHITE)
-    textRect = (WIDTH - 350 + 15 , (HEIGHT - 250)+(50/4))
-    
+    text_rect = (WIDTH - 350 + 15, (HEIGHT - 250) + (50 / 4))
+
     if (WIDTH - 350 + 100) > mouse[0] > (WIDTH - 350) and (HEIGHT - 250 + 50) > mouse[1] > (HEIGHT - 250):
         rect_stop = pygame.draw.rect(screen, LIGHT_RED, (WIDTH - 350, HEIGHT - 250, 100, 50), 0)
-        #get_click = pygame.mouse.get_pressed()
+        # get_click = pygame.mouse.get_pressed()
         if clicked:
             pygame.quit()
             exit()
     else:
         rect_stop = pygame.draw.rect(screen, RED, (WIDTH - 350, HEIGHT - 250, 100, 50), 0)
-    screen.blit(restart_text, textRect)
+    screen.blit(restart_text, text_rect)
 
     pygame.display.update()
 
     if enter:
-        state = 1
-        score = 0
+        sta = 1
+        scr = 0
 
-    return state, score
+    return sta, scr
 
-#Game loop
+
+# Game loop
 while True:
 
     get_click = False
@@ -263,29 +262,26 @@ while True:
         if event.type == pygame.QUIT:
             print("Exiting...")
             pygame.quit()
-            exit()     
+            exit()
         elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
             get_click = True
-            #print("CLICK")
+            # print("CLICK")
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
             enterPressed = True
 
-    #Start Menu
+    # Start Menu
     if state == 0:
         score = 0
-        state = start_screen(state, get_click, enterPressed)
-    #Game 
+        state = start_screen(get_click, enterPressed)
+    # Game
     elif state == 1:
         result = run_game(enemy_list, score)
         state = result[0]
         score = result[1]
-    #Death Menu
+    # Death Menu
     elif state == 2:
         result = dead_player(score, get_click, enterPressed)
         state = result[0]
-        score = result[1] 
-        
-    clock.tick(60)   
+        score = result[1]
 
-
-    
+    clock.tick(60)
